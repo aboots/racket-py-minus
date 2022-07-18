478e98658bbccf9c3e477ec5ddab92d4548a5c50
aboots <mahdi.abootorabi2@gmail.com>
2022-07-18 12:07:55 +0430

    add grammar and parser

---
+++ py-grammar.rkt
@@ -0,0 +1,232 @@
+#lang eopl
+
+(provide (all-defined-out))
+
+;1. Program → Statements EOF
+(define-datatype program program?
+  (a-program (statements statements?))
+  (checked-program
+   (checked string?)
+   (statements statements?)))
+
+;2. Statements → Statement ‘; ‘ | Statements Statement ‘; ‘
+(define-datatype statements statements?
+  (single-statements
+   (statement statement?))
+  (mult-statements
+   (statements statements?)
+   (statement statement?)))
+
+;3. Statement → Compound_stmt | Simple_stmt | Print
+(define-datatype statement statement?
+  (a-compound-stmt
+   (compound-stmt compound-stmt?))
+  (a-simple-stmt
+   (simple-stmt simple-stmt?))
+  (a-print-stmt
+   (print-stmt print?)))
+
+;4. Simple_stmt → Assignment | Return_stmt | Global_stmt
+(define-datatype simple-stmt simple-stmt?
+  (assignment-simple-stmt
+   (assignment assignment?))
+  (return-simple-stmt
+   (return-stmt return-stmt?))
+  (global-simple-stmt
+   (global-stmt global-stmt?))
+  (pass-stmt)
+  (break-stmt)
+  (continue-stmt))
+
+;5. Compound_stmt → Function_def | If_stmt | For_stmt
+(define-datatype compound-stmt compound-stmt?
+  (func-def-comp-stmt
+   (func-def function-def?))
+  (if-comp-stmt
+   (if-stmt if-stmt?))
+  (for-comp-stmt
+   (for-stmt for-stmt?)))
+
+;6. Assignment → ID ‘ = ‘ Expression
+(define-datatype assignment assignment?
+  (an-assignment
+   (ID symbol?)
+   (exp expression?)))
+
+;7. Return_stmt → ‘return‘ | ‘return‘ Expression
+(define-datatype return-stmt return-stmt?
+  (empty-return-stmt)
+  (exp-return-stmt
+   (exp expression?)))
+
+;8. Global_stmt → ‘global‘ ID
+(define-datatype global-stmt global-stmt?
+  (a-global-stmt
+   (ID symbol?)))
+
+;9. Function_def → ‘def‘ ID ‘(‘ Params ‘)‘ ‘ : ‘ Statements|‘def‘ ID ‘() : ‘ Statements
+(define-datatype function-def function-def?
+  (params-func-def
+   (ID symbol?)
+   (params params?)
+   (statements statements?))
+  (zero-param-func-def
+   (ID symbol?)
+   (statements statements?)))
+
+;10. Params → Param_with_default | Params ‘, ‘ Param_with_default
+(define params? (lambda (e) (and (not (null? e)) (list-of param-with-default?))))
+
+;11. Param_with_default → ID ‘ = ‘ Expression
+(define-datatype param-with-default param-with-default?
+  (a-param-with-default
+   (ID symbol?)
+   (exp expression?)))
+
+;12. If_stmt → ‘if‘ Expression ‘ : ‘ Statements Else_block
+(define-datatype if-stmt if-stmt?
+  (an-if-stmt
+   (exp expression?)
+   (statements statements?)
+   (else-block else-block?)))
+
+;13. Else_block → ‘else‘ ‘ : ‘ Statements
+(define-datatype else-block else-block?
+  (an-else-block
+   (statements statements?)))
+
+;14. For_stmt → ‘for‘ ID ‘in‘ Expression ‘ : ‘ Statements
+(define-datatype for-stmt for-stmt?
+  (a-for-stmt
+   (ID symbol?)
+   (exp expression?)
+   (statements statements?)))
+
+;15. Expression → Disjunction
+(define-datatype expression expression?
+  (an-expression
+   (disjunction disjunction?)))
+
+;16. Disjunction → Conjunction | Disjunction ‘or‘ Conjunction
+(define-datatype disjunction disjunction?
+  (single-disjunction
+   (conjunction conjunction?))
+  (mult-disjunction
+   (disjunction disjunction?)
+   (conjunction conjunction?)))
+
+;17. Conjunction → Inversion | Conjunction ‘and‘ Inversion
+(define-datatype conjunction conjunction?
+  (single-conjunction
+   (inversion inversion?))
+  (mult-conjunction
+   (conjunction conjunction?)
+   (inversion inversion?)))
+
+;18. Inversion → ‘not‘ Inversion | Comparison
+(define-datatype inversion inversion?
+  (not-inversion
+   (invresion inversion?))
+  (a-comparison
+   (comparison comparison?)))
+
+;19. Comparison → eq-sum | lt-sum | gt-sum | sum
+(define-datatype comparison comparison?
+  (single-comparison (sum sum?))
+  (eq-sum-comparison (eq-sum eq-sum?))
+  (lt-sum-comparison (lt-sum lt-sum?))
+  (gt-sum-comparison (gt-sum gt-sum?)))
+
+;20. Eq_Sum → Sum ‘ == ‘ Sum
+(define-datatype eq-sum eq-sum?
+  (an-eq-sum
+   (sum1 sum?)
+   (sum2 sum?)))
+
+;21. Lt_Sum → Sum ‘ < ‘ Sum
+(define-datatype lt-sum lt-sum?
+  (an-lt-sum
+   (sum1 sum?)
+   (sum2 sum?)))
+
+;22. Gt_Sum → Sum ‘ > ‘ Sum
+(define-datatype gt-sum gt-sum?
+  (a-gt-sum
+   (sum1 sum?)
+   (sum2 sum?)))
+
+;23. Sum → Sum ‘ + ‘ Term | Sum ‘ - ‘ Term | Term
+(define-datatype sum sum?
+  (add-sum
+   (sum sum?)
+   (term term?))
+  (sub-sum
+   (sum sum?)
+   (term term?))
+  (single-sum
+   (term term?)))
+
+;24. Term → Term ‘ ∗ ‘ Factor | Term ‘/‘ Factor | Factor
+(define-datatype term term?
+  (mul-term
+   (term term?)
+   (factor factor?))
+  (div-term
+   (term term?)
+   (factor factor?))
+  (single-term
+   (factor factor?)))
+   
+;25. Factor → ‘ + ‘ Power | ‘ - ‘ Power | Power
+(define-datatype factor factor?
+  (pos-factor
+   (power power?))
+  (neg-factor
+   (power power?))
+  (single-factor
+   (power power?)))
+  
+;26. Power → Atom ‘ ∗ ∗‘ Factor | Primary
+(define-datatype power power?
+  (a-pow
+   (atom atom?)
+   (factor factor?))
+  (a-primary
+   (primary primary?)))
+   
+;27. Primary → Atom | Primary ‘[‘ Expression ‘]‘ | Primary ‘()‘| Primary ‘(‘ Arguments ‘)‘
+(define-datatype primary primary?
+  (an-atom
+   (atom atom?))
+  (index-access
+   (primary primary?)
+   (exp expression?))
+  (zero-arg-func-call
+   (primary primary?))
+  (args-func-call
+   (primary primary?)
+   (arguments arguments?)))
+
+;28. Arguments → Expression | Arguments ‘, ‘ Expression
+(define arguments? (lambda (e) (and (not (null? e)) (list-of expression?))))
+
+;29. Atom → ID | ‘True‘ | ‘False‘ | ‘None‘ | NUMBER | List
+(define atom?
+  (lambda (a) (or (symbol? a) (boolean? a) (none? a) (number? a) (py-list? a))))
+
+(define-datatype none none?
+  (a-none))
+
+;30. List → ‘[‘ Expressions ‘]‘ | ‘[]‘
+(define py-list? (list-of expression?))
+
+;31. Expressions → Expressions ‘, ‘ Expression | Expression
+(define expressions? (lambda (e) (and (not (null? e)) (list-of expression?))))
+
+;32. Print → ‘print (‘ Atom ‘)‘ | ‘print (‘ Atoms ‘)‘
+;where Atoms → Atom | Atoms‘,‘ Atom
+(define-datatype print print?
+  (print-atom
+   (atom atom?))
+  (print-atoms
+   (atoms (list-of atom?))))
--- py-parser.rkt
+++ py-parser.rkt
@@ -1,19 +1,18 @@
 #lang racket
-(require parser-tools/lex
-         (prefix-in : parser-tools/lex-sre))
-#lang racket
 
 (require parser-tools/yacc)
 
-(require "lexer.rkt"
-         "grammar.rkt")
+(require "py-lexer.rkt"
+         "py-grammar.rkt")
+
+(provide (all-defined-out))
 
 (define py-parser
   (parser
    (start statements)
    (end EOF)
-   (error (lambda (tok-ok? tok-name tok-value) (error 'parser "problem parsing '~s ~s'" tok-name tok-value)))
-   (tokens a b)
+   (error void)
+   (tokens token-type-a token-type-b)
    (grammar
     (statements
      ((statement SEMICOL) (single-statements $1))
@@ -24,11 +23,11 @@
      ((print) (a-print-stmt $1)))
     (simple-stmt
      ((assignment) (assignment-simple-stmt $1))
-     ((return-stmt) (return-simple-stmt $1))
      ((global-stmt) (global-simple-stmt $1))
+     ((return-stmt) (return-simple-stmt $1))
      ((PASS) (pass-stmt))
-     ((CONTINUE) (continue-stmt))
      ((BREAK) (break-stmt)))
+     ((CONTINUE) (continue-stmt))
     (compound-stmt
      ((function-def) (func-def-comp-stmt $1))
      ((if-stmt) (if-comp-stmt $1))
@@ -66,10 +65,10 @@
      ((NOT inversion) (not-inversion $2))
      ((comparison) (a-comparison $1)))
     (comparison
-     ((eq-sum) $1)
-     ((lt-sum) $1)
-     ((gt-sum) $1)
-     ((sum) $1))
+     ((eq-sum) (eq-sum-comparison $1))
+     ((lt-sum) (lt-sum-comparison $1))
+     ((gt-sum) (gt-sum-comparison $1))
+     ((sum) (single-comparison $1)))
     (eq-sum
      ((sum EQUAL sum) (an-eq-sum $1 $3)))
     (lt-sum
@@ -119,4 +118,4 @@
      ((atom COMMA atom) (list $1 $3))
      ((atoms COMMA atom) (append $1 (list $3)))))))
 
-(define lex-and-parse (lambda (input) (a-program (py-parser (lex input)))))
+(define (lex-and-parse input) (a-program (py-parser (lex input))))
